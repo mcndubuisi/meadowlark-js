8ee1596bcc0ecf50b2a64a54ccf797e59f54cc1a
Ndubuisi <mcndubuisi@icloud.com>
2018-06-03 21:01:32 +0100

    Display request headers

--- meadowlark.js
+++ meadowlark.js
@@ -13,50 +13,59 @@ app.use(express.static(__dirname + '/public'));
 
 // Set up handlebars view engine
 var handlebars = require('express3-handlebars')
-    .create({defaultLayout: 'main'});
-    app.engine('handlebars', handlebars.engine);
-    app.set('view engine', 'handlebars');
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 // Middleware to detect test=1 in the querystring
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     next();
 });
 // Home Page
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.render('home');
 });
 
 // About Page
-app.get('/about', function(req, res){
+app.get('/about', function(req, res) {
     res.render('about', {
-            fortune: fortune.getFortune(), 
-            pageTestScript: './qa/tests-about.js',
-            pageTitle: 'About'
+        fortune: fortune.getFortune(),
+        pageTestScript: './qa/tests-about.js',
+        pageTitle: 'About'
     });
 });
 
-app.get('/tours/hood-river', function(req, res){
+app.get('/tours/hood-river', function(req, res) {
     res.render('tours/hood-river');
 });
 
-app.get('/tours/request-group-rate', function(req, res){
+app.get('/tours/request-group-rate', function(req, res) {
     res.render('tours/request-group-rate');
 });
 
+app.get('/headers', function(req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) {
+        s += name + ': ' + req.headers[name] + '\n';
+    }
+    res.send(s);
+});
+
 // 404 catch-all handler (middleware)
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
     res.status(404);
     res.render('404');
 });
 
 // 500 error handler (middleware)
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
     console.log('Express is running at http://localhost:' + app.get('port') + '...');
 });
